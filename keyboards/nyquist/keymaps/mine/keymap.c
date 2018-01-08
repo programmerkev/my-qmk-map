e4a8a83633592a03582136283be4d7f768576369
Kevin Mitchell <programmerkev@gmail.com>
2018-01-07 19:45:28 -0500

    Programmer Dvorak style numbers and move esc and enter keys

--- keyboards/nyquist/keymaps/mine/keymap.c
+++ keyboards/nyquist/keymaps/mine/keymap.c
@@ -76,30 +76,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  $   |  &   |  [   |   {  |   }  |   (  |   =  |   *  |   )  |   +  |   ]  |   #  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   "  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Ctrl |
+ * | Tab  |   ;  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Ctrl |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  _   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+ * | Shift|   '  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Adjust| Ctrl | Alt  |Lower | GUI  | Bksp |Space | Esc  |Raise | Down |  Up  |Esc   |
+ * |Adjust| Ctrl | Alt  | GUI  | Lower| Bksp |Space | Raise|Raise | Down |  Up  |Esc   |
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = KEYMAP( \
   KC_DLR,  KC_AMPR, KC_LBRC, KC_LCBR, KC_RCBR, KC_LPRN, KC_EQL,  KC_ASTR, KC_RPRN, KC_PLUS, KC_RBRC, KC_HASH, \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_RCTL, \
+  KC_TAB,  KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_RCTL, \
   KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINUS, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT , \
-  ADJUST,  KC_LCTL, KC_LALT, LOWER,   KC_LGUI, KC_BSPC, KC_SPC,  KC_ESC,  RAISE,   KC_DOWN, KC_UP,   KC_ESC \
+  KC_LSFT, KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT , \
+  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_SPC,  RAISE,   RAISE,   KC_DOWN, KC_UP,   KC_ESC \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   +  |Right |  |   |
+ * |   ~  |   %  |   7  |   5  |   3  |   1  |   9  |   0  |   2  |   4  |   6  |  8   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  | Left | Left |  Up  |  /   |  =   |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  Esc |  F5  | Left | Left |  Up  |  /   |  =   |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  | Down |  Up  |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = KEYMAP( \
   KC_TILD, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_RIGHT,KC_PIPE, \
-  KC_DEL,  KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_LEFT, KC_UP,   KC_SLASH,KC_EQL,  KC_BSLASH, \
+  KC_TILD, KC_PERC, KC_7,   KC_5,    KC_3,    KC_1,    KC_9,    KC_0,    KC_2,    KC_4,    KC_6,    KC_8, \
+  KC_DEL,  KC_F1,   KC_F2,  KC_F3,   KC_ESC,  KC_F5,   KC_LEFT, KC_LEFT, KC_UP,   KC_SLASH,KC_EQL,  KC_BSLASH, \
   _______, KC_F7,   KC_F8,  KC_DOWN, KC_UP,   KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______, \
   _______, _______, _______, _______, _______, _______,KC_ESC, KC_MNXT, _______, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
@@ -118,9 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   `  |   F1 |  F2  |  (   |  )   |   F5 |  F6  |   7  |   8  |   9  |   0  | Del  |
+ * |   ~  |   %  |   7  |   5  |   3  |   1  |   9  |   0  |   2  |   4  |   6  |  8   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  {   |  }   |   "  |  )   |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  F1  |  F2  |  {   |  }   |   "  |  )   | Enter|   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  [   |  ]   |  F11 |  F12 |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -129,10 +129,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_GRV,  KC_F1,   KC_F2,   KC_LPRN, KC_RPRN, KC_F5,   KC_F6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_LCBR, KC_RCBR, KC_DQUO, KC_QUOT, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, \
+  KC_TILD, KC_PERC, KC_7,   KC_5,    KC_3,    KC_1,    KC_9,    KC_0,    KC_2,    KC_4,    KC_6,    KC_8, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_LCBR, KC_RCBR, KC_DQUO, KC_QUOT, KC_ENT, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, KC_F7,   KC_F8,   KC_LBRC, KC_RBRC,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
-  _______, _______, _______, _______, _______, KC_ENT, _______, KC_MNXT, _______, KC_VOLD, KC_VOLU, KC_MPLY \
+  _______, _______, _______, _______, _______, KC_ENT, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Adjust (Lower + Raise)
